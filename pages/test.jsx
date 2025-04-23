cd55f6c63ec6c99ad02215ccbfd08ff9c253eb88
ArtmicRus <91434372+ArtmicRus@users.noreply.github.com>
2025-04-23 23:24:17 +0300

    Initial commit

---
+++ pages/test.jsx
@@ -0,0 +1,7 @@
+export default function TestPage(){
+    return (
+        <div className="underline text-2xl">
+            Hello world!
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      // tailwind очень оптимизированный так как генерируется только то что используется
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Дирректрии где могут находиться tailwind классы
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      // Создание классов которых нет изначально в talewind
+      gridTemplateColumns: {
+        'game-field': 'repeat(3, 30px)'
+      },
+      gridTemplateRows: {
+        'game-field': 'repeat(3, 30px)'
+      }
+    },
+  },
+  plugins: [],
+}
